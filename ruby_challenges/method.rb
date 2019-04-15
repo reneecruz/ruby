f2219b005aafc73a085724e9d5aff396696b1c2e
Renée Cruz <reneebasscruz@gmail.com>
2019-04-14 22:42:52 -0400

    Refactored math method to pass in arguments.

--- ruby_challenges/method.rb
+++ ruby_challenges/method.rb
@@ -11,3 +11,12 @@ def two_plus_two
 end
 
 two_plus_two
+
+# refactored math method to pass in arguments
+def add(num1, num2)
+  number = num1 + num2
+  puts number
+end
+
+add(928, 1067)
+add(89, 33)
