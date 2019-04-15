e7fa469d96b9370c2a1c6878ca642bf6898485bd
Renée Cruz <reneebasscruz@gmail.com>
2019-04-14 22:28:30 -0400

    Always Three script refactored to be a Method.

--- ruby_challenges/always_three.rb
+++ ruby_challenges/always_three.rb
@@ -63,3 +63,12 @@ puts "Always #{final_number}"
 puts "Give me a number"
 first_number = gets.to_i
 puts 'Always' + (((first_number + 5) * 2 - 4) / 2 - first_number).to_s # math on a single line using order of operations
+
+# refactor to be a method
+def always_three
+  puts "Give me a number"
+  first_number = gets.to_i
+  puts 'Always' + (((first_number + 5) * 2 - 4) / 2 - first_number).to_s
+end
+
+always_three
